403a15708ce0d40d65bb9f4145fc8a228cbe3f48
Anshu kumari <anshukumari87883@gmail.com>
2026-01-28 10:58:25 +0530

    operators

--- operators.py.py
+++ operators.py.py
@@ -1,5 +1,5 @@
 # student 5 marks total number and percentage
-s=input("enter student name: ")
+n=input("enter student name: ")
 m=input("enter student class: ")
 s=input("enter student section: ")
 a=int(input("student marks in english: "))
@@ -7,45 +7,45 @@ b=int(input("student marks in c++:"))
 c=int(input("student marks in python: "))
 d=int(input("student marks in hindi: "))  
 e=int(input("student marks java:  "))
-m=a+b+c+d+e
-print("total marks",m)
-print("percentage",(m/500)*100)
+mark=a+b+c+d+e
+p=mark/500*100
+print("Student name",n,"\nClass",m,"\nSection",s,"\nTotal marks",mark,"Student percentage",p)
 
 
-#   3 number sum
-a=int(input("enter 1 number: "))
-b=int(input("enter 2 number: "))
-c=int(input("enter 3 number: "))
-d= a+b+c
-print("sum of three number:",d)
+# #   3 number sum
+# a=int(input("enter 1 number: "))
+# b=int(input("enter 2 number: "))
+# c=int(input("enter 3 number: "))
+# d= a+b+c
+# print("sum of three number:",d)
  
-#  3 number square
-a=int(input("enter number:"))
-print(a**2)
+# #  3 number square
+# a=int(input("enter number:"))
+# print(a**2)
  
-#   string format
+# #   string format
 
-a= input("enter a temperature: ")
-celsuis= float(a)
-fahrenheit = (celsuis * 9 / 5) + 32
-print("Temperature in Celsius:", celsuis)
-print("Temperature in Fahrenheit:", fahrenheit)
+# a= input("enter a temperature: ")
+# celsuis= float(a)
+# fahrenheit = (celsuis * 9 / 5) + 32
+# print("Temperature in Celsius:", celsuis)
+# print("Temperature in Fahrenheit:", fahrenheit)
 
 
-# Calculate quotient and remainder
+# # Calculate quotient and remainder
 
-num1 = int(input("Enter first number: "))
-num2 = int(input("Enter second number: "))
-quotient = num1 // num2
-remainder = num1 % num2
-print("Quotient:", quotient)
-print("Remainder:", remainder)
+# num1 = int(input("Enter first number: "))
+# num2 = int(input("Enter second number: "))
+# quotient = num1 // num2
+# remainder = num1 % num2
+# print("Quotient:", quotient)
+# print("Remainder:", remainder)
 
-# Calculate Simple Interest 
+# # Calculate Simple Interest 
 
-P = float(input("Enter Principal: "))
-R = float(input("Enter Rate: "))
-T = float(input("Enter Time: "))
-SI = (P * R * T) / 100
-print("Simple Interest:", SI)
+# P = float(input("Enter Principal: "))
+# R = float(input("Enter Rate: "))
+# T = float(input("Enter Time: "))
+# SI = (P * R * T) / 100
+# print("Simple Interest:", SI)
 
